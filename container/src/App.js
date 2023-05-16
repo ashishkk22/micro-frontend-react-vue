275a0875396b2f514fb4cc817817ddf063c5d5c5
ashishkk22 <ashishkachhadiya22@gmail.com>
2023-05-16 15:35:14 +0530

    added header in the conatiner

--- container/src/App.js
+++ container/src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import MarketingApp from "./components/MarketingApp";
+import { BrowserRouter } from "react-router-dom";
+import Header from "./components/Header";
 
 const App = () => {
   return (
-    <>
-      <h1>Welcome to container</h1>
-      <hr />
+    <BrowserRouter>
+      <Header />
       <MarketingApp />
-    </>
+    </BrowserRouter>
   );
 };
 
